957e93243cedff679e8ccd7591f02bccfba592bd
Don't look at me <128672641+claindonstaff@users.noreply.github.com>
2023-03-23 06:47:30 +0000

    hello world

--- src/config.ts
+++ src/config.ts
@@ -36,13 +36,7 @@ export const SITE = {
     ytChannelId: '',
     // Optional, user/author settings (example)
     // Author: name
-    author: '', // Example: Fred K. Schott
-    // Author: Twitter handler
-    authorTwitter: '', // Example: FredKSchott
-    // Author: Image external source
-    authorImage: '', // Example: https://pbs.twimg.com/profile_images/1272979356529221632/sxvncugt_400x400.jpg, https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png
-    // Author: Bio
-    authorBio: 'Crisp, minimal, personal blog theme for Astro. Crisp, minimal, personal blog theme for Astro. Crisp, minimal, personal blog theme for Astro'
+    author: 'me',
 }
 
 // Ink - Theme configuration
---
+++ src/content/blog/hello-world.md
@@ -0,0 +1,12 @@
+---
+title: Hello world Astro
+date: "2023-01-01"
+author: me
+category: design
+tags:
+- front-end
+- me
+description: There is the first blog.
+---
+
+## Hello world!
